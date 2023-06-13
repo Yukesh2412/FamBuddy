e26025086cc408e4ce83ef7d27a656457fd434bb
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 <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>
2023-06-13 22:13:03 +0530

    node cron added

--- server.js
+++ server.js
@@ -130,10 +130,10 @@ wss.on("connection", function connection(ws) {
 
 // prevent render server from sleeping after 14 mins
 client.on("pong", (data) => {
-  console.log("NODE CRON - SELF PINGED", Date().toLocaleTimeString());
+  console.log("NODE CRON - SELF PINGED");
 });
 
-cron.schedule("*/10 * * * *", () => {
+cron.schedule("*/12 * * * *", () => {
   client.ping();
 });
 
