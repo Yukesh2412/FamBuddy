f4af9a064236b161a9498e30342ab3a0f2307b71
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 <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>
2023-06-13 16:15:29 +0530

    fixed rendering issues and refactoring done

--- frontend/fambuddy/App.js
+++ frontend/fambuddy/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useCallback, useState} from 'react';
+import React, {useEffect, useCallback, useState, useRef} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 import {GiftedChat} from 'react-native-gifted-chat';
@@ -6,9 +6,13 @@ import {GiftedChat} from 'react-native-gifted-chat';
 const Chat = ({}) => {
   const [messages, setMessages] = useState([]);
   const [isTyping, setisTyping] = useState(false);
-  const [isLoading, setisLoading] = useState(true);
+  const [isLoading, setisLoading] = useState();
+  const [isRetry, setisRetry] = useState(false);
+  const wsRef = useRef(null);
   var array = [];
+
   useEffect(() => {
+    setisLoading(true);
     setMessages([
       {
         _id: 1,
@@ -22,46 +26,53 @@ const Chat = ({}) => {
         },
       },
     ]);
-  }, []);
-
-  // const ws = new WebSocket('ws://192.168.1.19:3000');
-  const ws = new WebSocket('wss://fambuddy.onrender.com');
-
-  ws.onopen = () => {
-    console.log('connected to websocket');
-    setisLoading(false);
-  };
-
-  ws.onmessage = e => {
-    var response = e.data;
-    var data = [
-      {
-        _id: parseInt(Date.now() * Math.random()),
-        text: response,
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'Bot',
-          avatar:
-            'https://www.viralindiandiary.com/wp-content/uploads/2021/06/FamPay-Logo.png',
+    const ws = new WebSocket('ws://192.168.1.19:3000');
+    wsRef.current = ws;
+    ws.onopen = () => {
+      console.log('connected to websocket');
+      setisLoading(false);
+    };
+    ws.onmessage = e => {
+      var response = e.data;
+      var data = [
+        {
+          _id: parseInt(Date.now() * Math.random()),
+          text: response,
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'Bot',
+            avatar:
+              'https://www.viralindiandiary.com/wp-content/uploads/2021/06/FamPay-Logo.png',
+          },
         },
-      },
-    ];
-    setMessages(previousMessages => GiftedChat.append(previousMessages, data));
-    setisTyping(false);
-  };
+      ];
+      setMessages(previousMessages =>
+        GiftedChat.append(previousMessages, data),
+      );
+      setisTyping(false);
+    };
+    ws.onerror = e => {
+      console.log('websocket error', e.message);
+      setTimeout(() => {
+        setisRetry(!isRetry);
+      }, 10000);
+    };
 
-  ws.onerror = e => {
-    console.log('websocket error', e.message);
-  };
-
-  ws.onclose = e => {
-    console.log('websocket closed', e.code, e.reason);
-  };
+    ws.onclose = e => {
+      console.log('websocket closed', e.code, e.reason);
+      setTimeout(() => {
+        setisRetry(!isRetry);
+      }, 10000);
+    };
+    return () => {
+      ws.close();
+    };
+  }, [isRetry]);
 
   const onSend = useCallback((msg = []) => {
     try {
-      ws.send(
+      wsRef.current.send(
         JSON.stringify({
           previous: array,
           current: msg[0].text,
@@ -102,7 +113,7 @@ const Chat = ({}) => {
         />
       ) : (
         <View style={{alignSelf: 'center', marginVertical: '50%'}}>
-          <Text style={{fontSize: 20}}>Loading..</Text>
+          <Text style={{fontSize: 20}}>Connecting to FamBuddy..</Text>
         </View>
       )}
     </View>
@@ -123,3 +134,132 @@ const styles = StyleSheet.create({
 });
 
 export default Chat;
+
+// import React, {useEffect, useCallback, useState} from 'react';
+// import {View, Text, StyleSheet} from 'react-native';
+
+// import {GiftedChat} from 'react-native-gifted-chat';
+
+// const Chat = ({}) => {
+//   const [messages, setMessages] = useState([]);
+//   const [isTyping, setisTyping] = useState(false);
+//   const [isLoading, setisLoading] = useState();
+//   const [isRetry,setisRetry]=useState(false)
+//   var array = [];
+//   useEffect(() => {
+//     setisLoading(true);
+//     setMessages([
+//       {
+//         _id: 1,
+//         text: 'Hi there! I am FamBuddy!',
+//         createdAt: new Date(),
+//         user: {
+//           _id: 2,
+//           name: 'Bot',
+//           avatar:
+//             'https://www.viralindiandiary.com/wp-content/uploads/2021/06/FamPay-Logo.png',
+//         },
+//       },
+//     ]);
+//   }, [isRetry]);
+
+//   const ws = new WebSocket('ws://192.168.1.19:3000');
+//   // const ws = new WebSocket('wss://fambuddy.onrender.com');
+
+//   ws.onopen = () => {
+//     console.log('connected to websocket');
+//     setisLoading(false);
+//   };
+
+//   ws.onmessage = e => {
+//     var response = e.data;
+//     var data = [
+//       {
+//         _id: parseInt(Date.now() * Math.random()),
+//         text: response,
+//         createdAt: new Date(),
+//         user: {
+//           _id: 2,
+//           name: 'Bot',
+//           avatar:
+//             'https://www.viralindiandiary.com/wp-content/uploads/2021/06/FamPay-Logo.png',
+//         },
+//       },
+//     ];
+//     setMessages(previousMessages => GiftedChat.append(previousMessages, data));
+//     setisTyping(false);
+//   };
+
+//   ws.onerror = e => {
+//     console.log('websocket error', e.message);
+//   };
+
+//   ws.onclose = e => {
+//     console.log('websocket closed', e.code, e.reason);
+//     setisRetry(!isRetry);
+//   };
+
+//   const onSend = useCallback((msg = []) => {
+//     try {
+//       ws.send(
+//         JSON.stringify({
+//           previous: array,
+//           current: msg[0].text,
+//         }),
+//       );
+//       array.push(msg[0].text);
+//       setMessages(previousMessages => GiftedChat.append(previousMessages, msg));
+//       setisTyping(!isTyping);
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   }, []);
+
+//   return (
+//     <View style={{flex: 1}}>
+//       <View
+//         style={{
+//           flexDirection: 'row',
+//           alignSelf: 'center',
+//           margin: 10,
+//         }}>
+//         <Text style={styles.textStyle}>Fam</Text>
+//         <Text style={styles.textBuddy}>Buddy</Text>
+//       </View>
+//       {!isLoading ? (
+//         <GiftedChat
+//           // inverted={true}
+//           messages={messages}
+//           onSend={messages => onSend(messages)}
+//           user={{
+//             _id: 1,
+//             name: 'User',
+//           }}
+//           isTyping={isTyping}
+//           textInputProps={{
+//             placeholder: 'Type a message....',
+//           }}
+//         />
+//       ) : (
+//         <View style={{alignSelf: 'center', marginVertical: '50%'}}>
+//           <Text style={{fontSize: 20}}>Connecting to FamBuddy..</Text>
+//         </View>
+//       )}
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   textStyle: {
+//     color: '#ffac14',
+//     fontSize: 26,
+//     fontWeight: 'bold',
+//   },
+//   textBuddy: {
+//     color: 'black',
+//     fontSize: 26,
+//     fontWeight: 'bold',
+//   },
+// });
+
+// export default Chat;
