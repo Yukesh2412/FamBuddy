aa82a5b1b60389bae2d9695b9923884cb8fae627
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 <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>
2023-06-14 09:23:04 +0530

    cron shift

--- server.js
+++ server.js
@@ -137,6 +137,6 @@ wss.on("connection", function connection(ws) {
 //   client.ping();
 // });
 
-// server.listen(port, function () {
-//   console.log(`Server is listening on ${port}!`);
-// });
+server.listen(port, function () {
+  console.log(`Server is listening on ${port}!`);
+});
