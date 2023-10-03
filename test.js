40818e93a7ddc875c90a5fbcf0a5c49a888c5411
Yukesh <77680427+Yukesh2412@users.noreply.github.com>
2023-10-03 12:00:42 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+// Define your base URL
+const baseUrl = 'https://ags.hcm.bi';
+
+// Create a proxy middleware for all requests
+const apiProxy = createProxyMiddleware({ target: baseUrl, changeOrigin: true });
+app.use(apiProxy);
+
+// Optional: Add a route to specifically handle requests for the home page
+// app.get('/', (req, res) => {
+//   res.redirect(baseUrl);
+// });
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Proxy server is running on port ${port}`);
+});
